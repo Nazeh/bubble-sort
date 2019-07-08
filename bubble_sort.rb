fb10bdb093645207d2172d2d6febf65387be1acd
Nazeh <ar.nazeh@gmail.com>
2019-07-08 18:05:36 +0300

    finished bubble_sort_by

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -23,7 +23,7 @@ def bubble_sort_by(array)
     while n > 1
         newn = 0
         for i in 1..n-1
-            if array[i-1] > array[i]
+            if yield(array[i-1], array[i]) > 0
                 array[i-1], array[i] = array[i], array[i-1]
                 newn = i
             end
@@ -34,19 +34,19 @@ def bubble_sort_by(array)
     array
 end
 
-=begin
-puts bubble_sort_by(["hi","hello","hey"]) do |left,right|
-        left.length - right.length
-     end
-=end
 
 #test casses
-tests = [   [[2,3,4,1,26,9,10,4,4, 10, 15,12,2,0,1,5,70,23,57,29,0,12,33,100,33,77], [0, 0, 1, 1, 2, 2, 3, 4, 4, 4, 5, 9, 10, 10, 12, 12, 15, 23, 26, 29, 33, 33, 57, 70, 77, 100]],
-        ]
+tests = [
+    [[2,3,4,1,26,9,10,4,4, 10, 15,12,2,0,1,5,70,23,57,29,0,12,33,100,33,77], [0, 0, 1, 1, 2, 2, 3, 4, 4, 4, 5, 9, 10, 10, 12, 12, 15, 23, 26, 29, 33, 33, 57, 70, 77, 100]],
+]
 
-
+puts 'test Bubble_sort'
 for test in tests
     puts bubble_sort(test[0]) == test[1] ?  'success' : 'Failed',
         'expected:', test[1].to_s, 'Got:', bubble_sort(test[0]).to_s
     puts '  '
 end
+
+puts 'test Bubble_sort_BY'
+puts bubble_sort_by(["hi","hello","hey"]) { |left,right| left.length - right.length}.to_s
+puts bubble_sort_by(["hi","hello","hey"]) { |left,right| left.length - right.length}  == ["hi", "hey", "hello"]
