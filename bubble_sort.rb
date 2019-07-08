91a4a8332a5e345f3590a350ecef2785b58225e6
Nazeh <ar.nazeh@gmail.com>
2019-07-08 17:18:20 +0300

    bubble_sort works

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,12 +1,25 @@
 #bubble sort function
 def bubble_sort(array)
-    return array
+    n = array.length
+
+    while n >= 1
+        for i in 0...n-1
+            if array[i] > array[i+1]
+                frst = array[i]
+                array[i] = array[i+1]
+                array[i+1] = frst
+            end
+        end
+        
+        n -= 1
+    end
+
+    array
 end
 
+
 #test casses
-test1 =  bubble_sort([4,3,78,2,0,2])
-tests = [   [[4,3,78,2,0,2], [0,2,2,3,4,78]],
-            [["hi","hello","hey"], ["hi", "hey", "hello"]]
+tests = [   [[2,3,4,1,26,9,10,4,4, 10, 15,12,2,0,1,5,70,23,57,29,0,12,33,100,33,77], [0, 0, 1, 1, 2, 2, 3, 4, 4, 4, 5, 9, 10, 10, 12, 12, 15, 23, 26, 29, 33, 33, 57, 70, 77, 100]],
         ]
 
 
