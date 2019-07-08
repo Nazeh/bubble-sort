74f064c73fff585b2a0f79d757ccc06d570d8057
Nazeh <ar.nazeh@gmail.com>
2019-07-08 18:41:41 +0300

    rubocop linter

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,14 +1,16 @@
+# frozen_string_literal: true
+
 # bubble sort function
 def bubble_sort(array)
   n = array.length
   while n > 1
     newn = 0
-    for i in 1..n-1
-      if array[i-1] > array[i]
-        array[i-1], array[i] = array[i], array[i-1]
+    (1..n - 1).each do |i|
+      next unless array[i - 1] > array[i]
 
-        newn = i
-      end
+      array[i - 1], array[i] = array[i], array[i - 1]
+
+      newn = i
     end
     n = newn
   end
@@ -20,12 +22,12 @@ def bubble_sort_by(array)
   n = array.length
   while n > 1
     newn = 0
-    for i in 1..n-1
-      if yield(array[i-1], array[i]) > 0
-        array[i-1], array[i] = array[i], array[i-1]
+    (1..n - 1).each do |i|
+      next unless yield(array[i - 1], array[i]) > 0
 
-        newn = i
-      end
+      array[i - 1], array[i] = array[i], array[i - 1]
+
+      newn = i
     end
     n = newn
   end
@@ -34,14 +36,14 @@ end
 
 # test casses
 tests = [
-    [[2,3,4,1,26,9,10,4,4, 10, 15,12,2,0,1,5,70,23,57,29,0,12,33,100,33,77], [0, 0, 1, 1, 2, 2, 3, 4, 4, 4, 5, 9, 10, 10, 12, 12, 15, 23, 26, 29, 33, 33, 57, 70, 77, 100]],
+  [[2, 3, 4, 1, 26, 9, 10, 4, 4, 10, 15, 12, 2, 0, 1, 5, 70, 23, 57, 29, 0, 12, 33, 100, 33, 77], [0, 0, 1, 1, 2, 2, 3, 4, 4, 4, 5, 9, 10, 10, 12, 12, 15, 23, 26, 29, 33, 33, 57, 70, 77, 100]]
 ]
 puts 'test Bubble_sort'
-for test in tests
-    puts bubble_sort(test[0]) == test[1] ?  'success' : 'Failed',
-        'expected:', test[1].to_s, 'Got:', bubble_sort(test[0]).to_s
-    puts '  '
+tests.each do |test|
+  puts bubble_sort(test[0]) == test[1] ? 'success' : 'Failed',
+       'expected:', test[1].to_s, 'Got:', bubble_sort(test[0]).to_s
+  puts '  '
 end
 puts 'test Bubble_sort_BY'
-puts bubble_sort_by(["hi","hello","hey"]) { |left,right| left.length - right.length}.to_s
-puts bubble_sort_by(["hi","hello","hey"]) { |left,right| left.length - right.length}  == ["hi", "hey", "hello"]
+puts bubble_sort_by(%w[hi hello hey]) { |left, right| left.length - right.length }.to_s
+puts bubble_sort_by(%w[hi hello hey]) { |left, right| left.length - right.length } == %w[hi hey hello]
